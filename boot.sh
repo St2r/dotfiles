c04d1e37d3eac2975f3ae70197b353ad3e05b076
St2r <st2_r@qq.com>
2024-06-02 00:59:16 +0800

    zsh

--- boot.sh
+++ boot.sh
@@ -3,4 +3,5 @@ stow -t ~ yabai
 stow -t ~ skhd
 stow -t ~ sketchybar
 stow -t ~ nvim
+stow -t ~ zsh
 
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -213,20 +213,15 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	-- Detect tabstop and shiftwidth automatically
+	-- {
+	-- 	"tpope/vim-sleuth",
+	-- 	config = function()
+	-- 		require("tpope/vim-sleuth")
+	-- 	end,
+	-- },
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
