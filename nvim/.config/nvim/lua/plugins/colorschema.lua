5443a49c7e7b8c1633f2082a0d0eecdde6a0ca02
St2r <st2_r@qq.com>
2024-06-16 12:00:27 +0800

    transparent_background

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -3,6 +3,8 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     config = function()
+      -- compatition for transparent_background
+      vim.o.cursorlineopt = "number"
       require("catppuccin").setup({
         flavour = "mocha",
         transparent_background = true,
