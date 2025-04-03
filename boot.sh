52d944beba9d129df320c453e43864452fdbdc12
St2r <st2_r@qq.com>
2025-04-03 22:47:46 +0800

    feat: init.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,50 @@
-#!/bin/sh
+#!/usr/bin/env zsh
+
+# Output the current shell's bin path
+echo "Current shell bin path: $(which $SHELL)"
+
+# 1. install brew if not installed
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# 2. install brew packages
+packages=(
+    stow
+    skhd
+    yabai
+    sketchybar
+    btop
+    yazi
+    lazygit
+    neovim
+)
+
+# Install packages if not already installed
+for package in "${packages[@]}"; do
+    if ! brew list --formula $package &>/dev/null; then
+        echo "Installing $package..."
+        brew install $package
+    else
+        echo "$package already installed"
+    fi
+done
+
+## casks
+casks=(
+    hammerspoon
+    kitty
+)
+
+for cask in "${casks[@]}"; do
+    if ! brew list --cask $cask &>/dev/null; then
+        echo "Installing $cask..."
+        brew install --cask $cask
+    else
+        echo "$cask already installed"
+    fi
+done
+
 
 stow -t ~ kitty
 stow -t ~ yabai
